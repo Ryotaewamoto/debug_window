ef4a1109fbaa967e69beaf82c81b3fb53f2ae2fe
ryotaewamoto <lengtaiyanben@gmail.com>
2022-12-30 18:35:33 +0900

    init commit

---
+++ lib/content_text_styles.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+
+const contentTextStyle = TextStyle(
+  fontSize: 14,
+  fontWeight: FontWeight.w700,
+  color: Colors.white,
+);
---
+++ test/sample_test.dart
@@ -0,0 +1,18 @@
+// import 'package:flutter_test/flutter_test.dart';
+// import 'package:plugin_platform_interface/plugin_platform_interface.dart';
+
+void main() {
+  // final SamplePlatform initialPlatform = SamplePlatform.instance;
+
+  // test('$MethodChannelSample is the default instance', () {
+  //   expect(initialPlatform, isInstanceOf<MethodChannelSample>());
+  // });
+
+  // test('getPlatformVersion', () async {
+  //   Sample samplePlugin = Sample();
+  //   MockSamplePlatform fakePlatform = MockSamplePlatform();
+  //   SamplePlatform.instance = fakePlatform;
+
+  //   expect(await samplePlugin.getPlatformVersion(), '42');
+  // });
+}
